3a621c5c63c1ac632a72cf91807f84514270795a
Matthew Sullivan <matthew@msull92.com>
2017-11-07 19:06:50 -0600

    Improve max drawdown

--- src/stats.vue
+++ src/stats.vue
@@ -7,7 +7,7 @@
     <span class="label"><span class="period">{{ shared.period }}</span> Annualized</span> <span class="value" v-bind:class="portfolio_annualized_change_class">{{ formatted_portfolio_annualized_change }} ({{ portfolio_percent_change_annualized }}%)</span>
   </div>
   <div class="stat max_drawdown">
-    <span class="label">Max Drawdown</span><span class="value">{{ max_drawdown }}%</span>
+    <span class="label">Max Drawdown</span><span class="value" v-bind:class="max_drawdown_class">{{ formatted_max_drawdown }}%</span>
   </div>
   <div class="stat gain_to_pain_ratio">
     <span class="label">Gain / Pain Ratio</span><span class="value">{{ gain_to_pain_ratio }}</span>
@@ -47,11 +47,28 @@ export default {
 
       return sign + currency_formatter.format(amount);
     },
+    formatted_max_drawdown: function () {
+      let amount = this.max_drawdown;
+      let sign = "";
+
+      if (amount > 0) {
+        sign = "+"
+      }
+
+      return sign + currency_formatter.format(amount);
+    },
     portfolio_annualized_change_class: function() {
       return {
         is_positive: (this.portfolio_percent_change_annualized > 0),
         is_neutral: (this.portfolio_percent_change_annualized == 0),
         is_negative: (this.portfolio_percent_change_annualized < 0)
+      };
+    },
+    max_drawdown_class: function() {
+      return {
+        is_positive: (this.max_drawdown > 0),
+        is_neutral: (this.max_drawdown == 0),
+        is_negative: (this.max_drawdown < 0)
       };
     },
     portfolio_percent_change: function () {
@@ -100,7 +117,7 @@ export default {
     },
     max_drawdown: function() {
       let values = this.shared.portfolio_historical_values;
-      let result = analytics.maxDrawdown(values);
+      let result = -analytics.maxDrawdown(values);
 
       return Math.round(result * 10000) / 100;
     },
@@ -137,6 +154,22 @@ div#stats {
     span.value {
       text-align: right;
       width: 50%;
+    }
+  }
+
+  .max_drawdown {
+    span.value {
+      &.is_positive {
+        color: #98c379;
+      }
+
+      &.is_neutral {
+        color: #e5c07b;
+      }
+
+      &.is_negative {
+        color: #e05252;
+      }
     }
   }
 
